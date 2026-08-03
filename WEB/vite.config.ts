7c32c243f3abfa303992dd87879025f958ce82ca
Pixel <mohdarhamqazi@gmail.com>
2026-08-03 14:16:51 +0400

    Update vite.config.ts

--- WEB/vite.config.ts
+++ WEB/vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // Ensures assets load correctly on GitHub Pages
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
